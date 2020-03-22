1b4e238b2fdff0be981251aba4ebc1a3a6c264af
Dmitry <teramisu01@gmail.com>
2020-03-22 19:38:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let config = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        config[i] = bracketsConfig[i].join('');
+    }
+    
+    for (let i = 0; i < config.length; i++) {
+        while(str.includes(config[i])) {
+            str = str.replace(config[i], "");
+            i = 0;
+        }
+    }
+    return str.length === 0 ? true : false;
 }
