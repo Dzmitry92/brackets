4472d990259d76954dd7d1e6c43af06b64ebc498
Dmitry <teramisu01@gmail.com>
2020-10-13 22:47:06 +0300

    refactor: change structure of the task solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
-    let config = [];
-    for (let i = 0; i < bracketsConfig.length; i++) {
-        config[i] = bracketsConfig[i].join('');
-    }
+    let config = bracketsConfig.map((e) => e.join(''));
     
-    for (let i = 0; i < config.length; i++) {
+    for (let i = 0; i < config.length; i += 1) {
         while(str.includes(config[i])) {
             str = str.replace(config[i], "");
             i = 0;
